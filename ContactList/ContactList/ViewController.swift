2eea08edae854cb771c60cc1a78270078d7c468a
Orianka13 <odobrogor@gmail.com>
2022-12-19 14:30:42 +0400

    Создан проект

---
+++ ContactList/ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Олеся Егорова on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
