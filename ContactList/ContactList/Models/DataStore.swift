287186ef6dba6ba65788b64f5f9374e5ebdb92b4
Orianka13 <odobrogor@gmail.com>
2022-12-20 11:47:05 +0400

    Перегруппировка в соответствии с MVC

---
+++ ContactList/ContactList/Models/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Олеся Егорова on 20.12.2022.
+//
+
+import Foundation
---
+++ ContactList/ContactList/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Олеся Егорова on 20.12.2022.
+//
+
+import Foundation
