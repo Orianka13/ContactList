3c7283d0044b5f510951434ad01076589e8f57f9
Orianka13 <odobrogor@gmail.com>
2022-12-20 11:42:23 +0400

    Добавлены все основные сцены

---
+++ ContactList/ContactList/PersonInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PersonInfoViewController.swift
+//  ContactList
+//
+//  Created by Олеся Егорова on 20.12.2022.
+//
+
+import UIKit
+
+class PersonInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
