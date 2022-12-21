d1d618f3ea26bc7aeb40e056f704e1e5050f4ffa
Orianka13 <odobrogor@gmail.com>
2022-12-21 17:09:15 +0400

    Корректный вывод рандомных данных Person

--- ContactList/ContactList/Models/Person.swift
+++ ContactList/ContactList/Models/Person.swift
@@ -17,18 +17,26 @@ struct Person: Hashable {
 }
 
 extension Person {
+    
     static func getPersons() -> [Person] {
-        let dataStore = DataStore()
-        var persons: Set<Person> = []
         
-        for _ in 1...20 {
-            let person =   Person(firstName: dataStore.firstNames.randomElement() ?? "",
-                                  secondName: dataStore.secondNames.randomElement() ?? "",
-                                  email: dataStore.emails.randomElement() ?? "",
-                                  phoneNumber: dataStore.phoneNumbers.randomElement() ?? "")
-            persons.insert(person)
+        let dataStore = DataStore()
+        var persons: [Person] = []
+        
+        for _ in 0...dataStore.firstNames.endIndex - 1 {
+         
+            let firstName = dataStore.firstNames.remove(at: Int.random(in: dataStore.firstNames.startIndex...dataStore.firstNames.endIndex - 1))
+            let secondName = dataStore.secondNames.remove(at: Int.random(in: dataStore.secondNames.startIndex...dataStore.secondNames.endIndex - 1))
+            let phoneNumber = dataStore.phoneNumbers.remove(at: Int.random(in: dataStore.phoneNumbers.startIndex...dataStore.phoneNumbers.endIndex - 1))
+            let email = dataStore.emails.remove(at: Int.random(in: dataStore.emails.startIndex...dataStore.emails.endIndex - 1))
+            
+            let person = Person(firstName: firstName,
+                                secondName: secondName,
+                                email: email,
+                                phoneNumber: phoneNumber)
+            persons.append(person)
         }
         
-        return Array(persons)
+        return persons
     }
 }
