96c56fc173908ad32c28787eaf4929bbb22f70d3
Orianka13 <odobrogor@gmail.com>
2022-12-21 16:32:20 +0400

    Отображение данных в детальной таблице

--- ContactList/ContactList/ViewControllers/DetailPersonsListViewController.swift
+++ ContactList/ContactList/ViewControllers/DetailPersonsListViewController.swift
@@ -8,38 +8,51 @@
 import UIKit
 
 class DetailPersonsListViewController: UITableViewController {
+    
+    private let persons = Person.getPersons()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+       
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return persons.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        let person = persons[indexPath.section]
+        
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "phoneNumber", for: indexPath)
+            var content = cell.defaultContentConfiguration()
+            content.text = person.phoneNumber
+            content.image = UIImage(systemName: "phone")
+            cell.contentConfiguration = content
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "email", for: indexPath)
+            var content = cell.defaultContentConfiguration()
+            content.text = person.email
+            content.image = UIImage(systemName: "paperplane")
+            cell.contentConfiguration = content
+            return cell
+        }
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        persons[section].fullName
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
